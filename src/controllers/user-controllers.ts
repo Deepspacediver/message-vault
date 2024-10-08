00485a5f1006415c97562ef1494e8ac01e88d162
lukasz-mac <luka.koscik@gmail.com>
2024-10-08 10:59:45 +0200

    add controller to change user role

--- src/controllers/user-controllers.ts
+++ src/controllers/user-controllers.ts
@@ -1,11 +1,19 @@
 import asyncHandler from "express-async-handler";
 import {schemaRequestParser} from "../helpers/schema-parse.js";
-import {CreateUserSchema} from "../schemas/user-schemas.js";
-import {createUser} from "../db/user-queries.js";
+import {AssignUserRoleSchema, CreateUserSchema} from "../schemas/user-schemas.js";
+import {changeUserRole, createUser} from "../db/user-queries.js";
 
 export const createUserPost = asyncHandler(async (req, res) => {
     schemaRequestParser(CreateUserSchema, req);
     const {userName, firstName, lastName, role, password} = req.body;
     const data = await createUser({userName, firstName, role, password, lastName});
     res.json(data);
+});
+
+export const assignNewRolePut = asyncHandler(async (req, res) => {
+    schemaRequestParser(AssignUserRoleSchema, req);
+    const {role} = req.body;
+    const {userId} = req.params;
+    await changeUserRole(+userId, role);
+    res.status(200).json({message: `The role of the user has been changed to ${role}`});
 });
