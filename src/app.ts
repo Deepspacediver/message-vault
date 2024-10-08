109b96de828e152c99fccf8c01e6cf4c5a934483
lukasz-mac <luka.koscik@gmail.com>
2024-10-08 13:29:22 +0200

    add ejs as view engine & routers to app

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,23 @@
 import 'dotenv/config';
 import express from "express";
 import {errorMiddleware} from "./middlewares/error-middleware.js";
+import path from "path";
+import signUpRouter from "./routes/sign-up-router.js";
+import userRouter from "./routes/user-router.js";
+
+const __dirname = import.meta.dirname;
+
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "../src/views"));
+
+
+app.use("/sign-up", signUpRouter);
+app.use("/users", userRouter);
 
 app.use(errorMiddleware);
 
