908be1bd82db7f8dfd23824657a7665ea08dbd61
lukasz-mac <luka.koscik@gmail.com>
2024-10-08 10:58:32 +0200

    add AssignUserRoleSchema

--- src/schemas/user-schemas.ts
+++ src/schemas/user-schemas.ts
@@ -1,10 +1,12 @@
 import z from "zod";
 import {UserRoles} from "../types/user-types.js";
 
+const UserIdSchema = z.coerce.number({message: "User id must be a numer"});
 const UserNameSchema = z.coerce.string({message: "Username must be a string"}).trim();
 const FirstNameSchema = z.coerce.string({message: "First name must be a string"}).trim();
 const LastNameSchema = z.coerce.string({message: "Last name must be a string"}).trim();
 const PasswordSchema = z.coerce.string({message: "Password must be a string"}).trim();
+const UserRoleSchema = z.nativeEnum(UserRoles);
 
 export const CreateUserSchema = z.object({
     body: z.object({
@@ -13,5 +15,16 @@ export const CreateUserSchema = z.object({
         lastName: LastNameSchema,
         role: z.literal(UserRoles.USER),
         password: PasswordSchema,
+    })
+});
+
+export const AssignUserRoleSchema = z.object({
+    body: z.object({
+            role: UserRoleSchema
+        },
+    ),
+    params: z.object({
+        userId: UserIdSchema,
+
     })
 });
