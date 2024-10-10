1bf04833533980bc003116b06e6766beb705c41a
lukasz-mac <luka.koscik@gmail.com>
2024-10-10 09:54:23 +0200

    update password schema

--- src/schemas/user-schemas.ts
+++ src/schemas/user-schemas.ts
@@ -5,7 +5,8 @@ const UserIdSchema = z.coerce.number({message: "User id must be a numer"});
 const UserNameSchema = z.coerce.string({message: "Username must be a string"}).trim();
 const FirstNameSchema = z.coerce.string({message: "First name must be a string"}).trim();
 const LastNameSchema = z.coerce.string({message: "Last name must be a string"}).trim();
-const PasswordSchema = z.coerce.string({message: "Password must be a string"}).trim();
+const PasswordSchema = z.coerce.string({message: "Password must be a string"})
+    .min(5, "Password must be at least 5 characters long");
 const UserRoleSchema = z.nativeEnum(UserRoles);
 
 export const CreateUserSchema = z.object({
