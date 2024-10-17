839c5f4997983cb67718a0c1fd526ba028098550
lukasz-mac <luka.koscik@gmail.com>
2024-10-17 14:41:29 +0200

    add types for post & queries to fetch posts based on role

--- src/types/post-types.ts
+++ src/types/post-types.ts
@@ -3,3 +3,13 @@ export type CreatePost = {
     content: string,
     userId: number
 }
+
+export type Post = {
+    user_full_name: string,
+    created_at: Date,
+    post_id: number,
+    title: string,
+    content: string,
+}
+
+export type PostLimitedUserInfo = Omit<Post, 'user_full_name' | 'created_at'>
