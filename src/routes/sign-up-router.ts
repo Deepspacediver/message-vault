5d2576b93db4eeffec3c65e45af0006308655a30
lukasz-mac <luka.koscik@gmail.com>
2024-10-17 20:03:05 +0200

    redirect to homepage on signup/singin when user is logged in

--- src/routes/sign-up-router.ts
+++ src/routes/sign-up-router.ts
@@ -4,6 +4,9 @@ import {createUserPost} from "../controllers/sign-up-controller.js";
 const signUpRouter = Router();
 
 signUpRouter.get("/", (req, res) => {
+    if (!!req.user) {
+        return res.redirect('/');
+    }
     res.render("forms/sign-up-form");
 }).post("/", createUserPost);
 
