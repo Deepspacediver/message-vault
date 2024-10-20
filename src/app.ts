37df478125df9c7c8a00a304234fc47c63e9f0e6
lukasz-mac <luka.koscik@gmail.com>
2024-10-20 13:16:29 +0200

    check railway port

--- src/app.ts
+++ src/app.ts
@@ -89,6 +89,6 @@ app.use("", indexRouter);
 app.use(errorMiddleware);
 
 app.listen(process.env.PORT || 3000, () => {
-    console.log(`App listening at http://localhost:3000`);
+    console.log(`App listening at http://localhost:${process.env.PORT}`);
 });
 
